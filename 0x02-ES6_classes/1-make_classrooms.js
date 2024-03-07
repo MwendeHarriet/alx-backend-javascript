46a46614210b8c1a913eba936808b4b2216bd6e3
MwendeHarriet <mwendeharriet@gmail.com>
2024-03-07 13:03:00 +0000

    return 3 ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const cr1 = new ClassRoom(19);
+  const cr2 = new ClassRoom(20);
+  const cr3 = new ClassRoom(34);
+  return [cr1, cr2, cr3];
+}
