5e2bb9d9f6fe6ee05a82c9ed4a1622a342dfebd9
MwendeHarriet <mwendeharriet@gmail.com>
2024-03-07 16:17:33 +0000

    more cars

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    if (typeof range === 'string') this.new_range = range;
+    if (typeof range === 'string') this._range = range;
   }
 
   cloneCar() {
