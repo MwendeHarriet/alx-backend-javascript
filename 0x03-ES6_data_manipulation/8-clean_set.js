d66896c7e97900a301087c0ff2a2420dbc6fc422
MwendeHarriet <mwendeharriet@gmail.com>
2024-03-12 16:27:07 +0000

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function setFromArray(set, string) {
+  let x = '';
+  for (const i of set) {
+    if (i.startsWith(string)) x += `${i.slice(string.length)}-`;
+  }
+  return x.slice(0, -1);
+}
